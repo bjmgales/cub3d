39de834db1d594a94d2ec1ff00b8059d7dc61132
Charly Tardy <ctardy@c2r8p4.42nice.fr>
2023-06-27 16:51:27 +0200

    texture reverse

--- src/init-calcul/norm_calc.c
+++ src/init-calcul/norm_calc.c
@@ -6,7 +6,7 @@
 /*   By: ctardy <ctardy@student.42nice.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/23 20:37:44 by bgales            #+#    #+#             */
-/*   Updated: 2023/06/25 22:05:54 by ctardy           ###   ########.fr       */
+/*   Updated: 2023/06/27 16:49:43 by ctardy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,11 @@ void	last_calc_norm(t_game *game, t_calc *n, t_data img, double step)
 	double	tex_pos;
 
 	tex_pos = 0;
+		game->texig.tex_x = game->texig.texture_height - game->texig.tex_x;
 	if ((n->side == 0 && n->ray_dir_x > 0)
 		|| (n->side == 1 && n->ray_dir_y < 0))
-		game->texig.tex_x = game->texig.texture_height - game->texig.tex_x - 1;
+		game->texig.tex_x = game->texig.texture_width - game->texig.tex_x - 1;
+		
 	if (n->se_draw[0] < 0)
 	{
 		tex_pos += step * -n->se_draw[0];
